4d8d4755ee23b6e814d1b5bea4a555d8f9355c17
Richard Vogt <richvogtjr@gmail.com>
2013-07-15 15:41:32 -0500

    add bcrypt

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  has_many :listings
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end  
+
+end
+
+
+
+   
+
+
+
