70325a09363c5ddb622d4289ab79820b030ab0e0
Richard Vogt <richvogtjr@gmail.com>
2013-07-15 15:43:34 -0500

    add rest of bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,22 @@ class User < ActiveRecord::Base
     self.password_hash = @password
   end  
 
+  def create
+    @user = User.new(params[:user])
+    @user.password = params[:password]
+    @user.save!
+  end
+
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      give_token
+    else
+      redirect_to home_url
+    end
+  end
+
+
 end
 
 
